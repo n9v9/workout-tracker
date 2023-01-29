0c80f47f3e546f9d9830b8737a990a74b107e202
Niklas Vogel <21263864+n9v9@users.noreply.github.com>
2023-01-29 01:02:24 +0100

    Make UI full height and add bottom margin

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -7,35 +7,44 @@
     import Statistics from "../lib/components/Statistics.svelte";
 </script>
 
-<progress class="progress is-small mb-0 {!$isLoading ? 'is-invisible' : ''}" />
+<div class="app">
+    <progress class="progress is-small mb-0 {!$isLoading ? 'is-invisible' : ''}" />
 
-<main class="container px-3 pt-3">
-    {#if $apiErrorMessage !== ""}
-        <div class="notification is-danger is-light">
-            {$apiErrorMessage}
-        </div>
-    {/if}
+    <main class="container px-3 pt-3">
+        {#if $apiErrorMessage !== ""}
+            <div class="notification is-danger is-light">
+                {$apiErrorMessage}
+            </div>
+        {/if}
 
-    <Router>
-        <Route path="/" component={WorkoutList} />
-        <Route path="/workouts/:id" let:params>
-            <WorkoutInfo id={parseInt(params.id)} />
-        </Route>
-        <Route path="/workouts/:id/sets/add" let:params>
-            <SetForm workoutId={parseInt(params.id)} />
-        </Route>
-        <Route path="/workouts/:id/sets/:setId" let:params>
-            <SetForm workoutId={parseInt(params.id)} setId={parseInt(params.setId)} />
-        </Route>
-        <Route path="/statistiken">
-            <Statistics />
-        </Route>
-    </Router>
-</main>
+        <Router>
+            <Route path="/" component={WorkoutList} />
+            <Route path="/workouts/:id" let:params>
+                <WorkoutInfo id={parseInt(params.id)} />
+            </Route>
+            <Route path="/workouts/:id/sets/add" let:params>
+                <SetForm workoutId={parseInt(params.id)} />
+            </Route>
+            <Route path="/workouts/:id/sets/:setId" let:params>
+                <SetForm workoutId={parseInt(params.id)} setId={parseInt(params.setId)} />
+            </Route>
+            <Route path="/statistiken">
+                <Statistics />
+            </Route>
+        </Router>
+    </main>
+</div>
 
 <style>
+    .app {
+        width: 100%;
+        height: 100vh;
+    }
     progress {
         border-radius: 0;
         height: 0.5rem !important;
+    }
+    main {
+        margin-bottom: 1em;
     }
 </style>
