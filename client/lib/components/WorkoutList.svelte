86c49049486222577569155815e034a66c5d95bd
Niklas Vogel <21263864+n9v9@users.noreply.github.com>
2023-02-20 20:53:17 +0100

    Display date for the last 3 days specially

--- client/lib/components/WorkoutList.svelte
+++ client/lib/components/WorkoutList.svelte
@@ -36,15 +36,42 @@
     }
 
     function formatDate(date: Date): string {
-        return (
-            date.toLocaleString("de", {
+        const dateFormatted = date.toLocaleDateString("de", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+        });
+
+        const timeFormatted =
+            date.toLocaleTimeString("de", {
                 hour: "2-digit",
                 minute: "2-digit",
-                day: "2-digit",
-                month: "2-digit",
-                year: "numeric",
-            }) + " Uhr"
-        );
+            }) + " Uhr";
+
+        const isRelativeTo = (date: Date, relativeDay: number): boolean => {
+            const today = new Date();
+            return (
+                date.getDate() === today.getDate() + relativeDay &&
+                date.getMonth() === today.getMonth() &&
+                date.getFullYear() === today.getFullYear()
+            );
+        };
+
+        if (isRelativeTo(date, 0)) {
+            return `Heute, ${timeFormatted}`;
+        } else if (isRelativeTo(date, -1)) {
+            return `Gestern, ${timeFormatted}`;
+        }
+
+        for (let i = -2; i > -7; i--) {
+            if (isRelativeTo(date, i)) {
+                return `${date.toLocaleDateString("de", {
+                    weekday: "long",
+                })}, ${timeFormatted}`;
+            }
+        }
+
+        return `${dateFormatted}, ${timeFormatted}`;
     }
 </script>
 
